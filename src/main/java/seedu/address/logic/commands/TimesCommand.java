8f172a8bce7a82dae45fecab5a4f702b07589aca
Jiang <20010421jsy@gmail.com>
2020-10-04 23:38:36 +0800

    Fourth Iteration - UI Finalization

--- src/main/java/seedu/address/logic/commands/TimesCommand.java
+++ src/main/java/seedu/address/logic/commands/TimesCommand.java
@@ -2,47 +2,69 @@ package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIMES;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Times;
 
 /**
- * Changes the remark of an existing person in the address book.
+ * Changes the times of an existing book in the database.
  */
 public class TimesCommand extends Command {
-
     public static final String COMMAND_WORD = "times";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the times of the person identified "
             + "by the index number used in the last book listing. "
             + "Existing times will be overwritten by the input.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + PREFIX_TIMES + "[TIMES]\n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_TIMES + "1";
-
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Times: %2$s";
-
+            + PREFIX_TIMES + "Likes to swim.";
+    
+    public static final String MESSAGE_ADD_TIMES_SUCCESS = "Added times to Book: %1$s";
+    public static final String MESSAGE_DELETE_TIMES_SUCCESS = "Removed times from Book: %1$s";
 
     private final Index index;
     private final Times times;
-
     /**
-     * @param index of the book in the filtered book list to edit the remark
-     * @param times of the book to be updated to
+     * @param index of the book in the filtered book list to edit the times
+     * @param times of the book being borrowed to be updated to
      */
     public TimesCommand(Index index, Times times) {
         requireAllNonNull(index, times);
-
         this.index = index;
         this.times = times;
     }
-    
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), times));
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), times, personToEdit.getTags());
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether the times is added to or removed from
+     * {@code bookToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !times.value.isEmpty() ? MESSAGE_ADD_TIMES_SUCCESS : MESSAGE_DELETE_TIMES_SUCCESS;
+        return String.format(message, personToEdit);
     }
 
     @Override
@@ -51,12 +73,10 @@ public class TimesCommand extends Command {
         if (other == this) {
             return true;
         }
-
         // instanceof handles nulls
         if (!(other instanceof TimesCommand)) {
             return false;
         }
-
         // state check
         TimesCommand e = (TimesCommand) other;
         return index.equals(e.index)
--- src/test/java/seedu/address/logic/commands/TimesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/TimesCommandTest.java
@@ -6,31 +6,104 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TIMES_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TIMES_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.TimesCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Times;
+import seedu.address.testutil.PersonBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for TimesCommand.
  */
 public class TimesCommandTest {
 
+    private static final String TIMES_STUB = "Some times";
+
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute() {
-        final String times = "Some times";
+    public void execute_addRemarkUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withTimes(TIMES_STUB).build();
 
-        assertCommandFailure(new TimesCommand(INDEX_FIRST_PERSON, new Times(times)), model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), times));
+        TimesCommand remarkCommand = new TimesCommand(INDEX_FIRST_PERSON, new Times(editedPerson.getTimes().value));
+
+        String expectedMessage = String.format(TimesCommand.MESSAGE_ADD_TIMES_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_deleteRemarkUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(firstPerson).withTimes("").build();
+
+        TimesCommand remarkCommand = new TimesCommand(INDEX_FIRST_PERSON,
+                new Times(editedPerson.getTimes().toString()));
+
+        String expectedMessage = String.format(TimesCommand.MESSAGE_DELETE_TIMES_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()))
+                .withTimes(TIMES_STUB).build();
+
+        TimesCommand remarkCommand = new TimesCommand(INDEX_FIRST_PERSON, new Times(editedPerson.getTimes().value));
+
+        String expectedMessage = String.format(TimesCommand.MESSAGE_ADD_TIMES_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, editedPerson);
+
+        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        TimesCommand remarkCommand = new TimesCommand(outOfBoundIndex, new Times(VALID_TIMES_BOB));
+
+        assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        TimesCommand remarkCommand = new TimesCommand(outOfBoundIndex, new Times(VALID_TIMES_BOB));
+
+        assertCommandFailure(remarkCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
         
     @Test
@@ -53,7 +126,7 @@ public class TimesCommandTest {
         // different index -> returns false
         assertFalse(standardCommand.equals(new TimesCommand(INDEX_SECOND_PERSON, new Times(VALID_TIMES_AMY))));
 
-        // different remark -> returns false
+        // different times -> returns false
         assertFalse(standardCommand.equals(new TimesCommand(INDEX_FIRST_PERSON, new Times(VALID_TIMES_BOB))));
     }
 }
