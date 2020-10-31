82cb619c363ced7b08d8af78b951e6ede34dbea5
hcy <2320032466@qq.com>
2020-10-31 13:18:03 +0800

    Implement the review

--- src/main/java/seedu/address/logic/commands/AddReviewCommand.java
+++ src/main/java/seedu/address/logic/commands/AddReviewCommand.java
@@ -63,24 +63,26 @@ public class AddReviewCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Book> lastShownList = model.getFilteredBookList();
+        try {
+            requireNonNull(model);
+            List<Book> lastShownList = model.getFilteredBookList();
+            if (index.getZeroBased() > lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_BOOK_DISPLAYED_INDEX);
+            }
+            Book bookToReview = lastShownList.get(index.getZeroBased());
+            Book reviewedBook = createdChangedBook(bookToReview, review);
 
-        if (index.getZeroBased() > lastShownList.size()) {
+            model.setBook(bookToReview, reviewedBook);
+
+            List<String> keywords = new ArrayList<>(Arrays.asList((reviewedBook.getName().fullName).split(" ")));
+
+            NameMatchesKeywordPredicate nameMacthedKeywordsPredicate = new NameMatchesKeywordPredicate(keywords);
+            model.updateFilteredBookList(nameMacthedKeywordsPredicate, Mode.REVIEW);
+
+            return new CommandResult(String.format(MESSAGE_ADD_REVIEW_SUCCESS, reviewedBook));
+        } catch (Exception exception) {
             throw new CommandException(Messages.MESSAGE_INVALID_BOOK_DISPLAYED_INDEX);
         }
-
-        Book bookToReview = lastShownList.get(index.getZeroBased());
-        Book reviewedBook = createdChangedBook(bookToReview, review);
-
-        model.setBook(bookToReview, reviewedBook);
-
-        List<String> keywords = new ArrayList<>(Arrays.asList((reviewedBook.getName().fullName).split(" ")));
-
-        NameMatchesKeywordPredicate nameMacthedKeywordsPredicate = new NameMatchesKeywordPredicate(keywords);
-        model.updateFilteredBookList(nameMacthedKeywordsPredicate, Mode.REVIEW);
-
-        return new CommandResult(String.format(MESSAGE_ADD_REVIEW_SUCCESS, reviewedBook));
     }
 
     private static Book createdChangedBook(Book book, Review review) {
