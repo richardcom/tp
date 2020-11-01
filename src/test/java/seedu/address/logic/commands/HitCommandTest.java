345c2d13b90938ac821d0db51fffcc7c17c6045a
Caiyi34777 <e0376971@u.nus.edu>
2020-11-01 16:48:44 +0800

    Add hit command test

--- src/test/java/seedu/address/logic/commands/HitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HitCommandTest.java
@@ -1,4 +1,20 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.HitCommand.SHOWING_HIT_MESSAGE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
 public class HitCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_hit_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_HIT_MESSAGE);
+        assertCommandSuccess(new HitCommand(), model, expectedCommandResult, expectedModel);
+    }
 }
