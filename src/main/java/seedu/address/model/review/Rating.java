76caca0a8f8bcd286cf8a2f7dcc2b1f1e48e5ccb
Caiyi34777 <e0376971@u.nus.edu>
2020-11-01 22:01:48 +0800

    fix rating range problem and add testcases

--- src/main/java/seedu/address/model/review/Rating.java
+++ src/main/java/seedu/address/model/review/Rating.java
@@ -29,7 +29,13 @@ public class Rating {
      * Returns true if a given string is a valid rating.
      */
     public static boolean isValidRating(String test) {
-        return test.matches(VALIDATION_REGEX);
+        try {
+            int value = Integer.parseInt(test);
+            return value >= 0 && value <= 5;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        //return test.matches(VALIDATION_REGEX);
     }
 
     @Override
--- src/test/java/seedu/address/model/review/RatingTest.java
+++ src/test/java/seedu/address/model/review/RatingTest.java
@@ -6,8 +6,24 @@ import org.junit.jupiter.api.Test;
 class RatingTest {
 
     @Test
-    void isValidRating() {
+    void isValidRating_inRange_success() {
         Assertions.assertTrue(Rating.isValidRating("5"));
-        Assertions.assertFalse(Rating.isValidRating("a"));
+        Assertions.assertTrue(Rating.isValidRating("2"));
+        Assertions.assertTrue(Rating.isValidRating("0"));
     }
+
+    @Test
+    void isValidRating_outOfRange_failure() {
+        Assertions.assertFalse(Rating.isValidRating("6"));
+        Assertions.assertFalse(Rating.isValidRating("-1"));
+        Assertions.assertFalse(Rating.isValidRating("100"));
+    }
+
+    @Test
+    void isValidRating_wrongType_failure() {
+        Assertions.assertFalse(Rating.isValidRating("a"));
+        Assertions.assertFalse(Rating.isValidRating("\n"));
+        Assertions.assertFalse(Rating.isValidRating("1.0"));
+    }
+
 }
