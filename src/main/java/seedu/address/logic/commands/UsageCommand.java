b3192805196a622a6906e4918c401ebaaa0f265a
Caiyi34777 <e0376971@u.nus.edu>
2020-11-01 13:14:34 +0800

    Javadoc for UsageCommand

--- src/main/java/seedu/address/logic/commands/UsageCommand.java
+++ src/main/java/seedu/address/logic/commands/UsageCommand.java
@@ -15,13 +15,23 @@ import seedu.address.model.book.Book;
  */
 public class UsageCommand extends Command {
     public static final String COMMAND_WORD = "usage";
-
     private final Index targetIndex;
 
+    /**
+     * Constructor for UsageCommand.
+     * @param targetIndex index from user input
+     */
     public UsageCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
     }
 
+    /**
+     * Execute usage command on model and return with result.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return a new CommandResult object
+     * @throws CommandException if size is not legal
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
@@ -37,14 +47,32 @@ public class UsageCommand extends Command {
                 getUsage(lastShownList.get(targetIndex.getZeroBased()))));
     }
 
+    /**
+     * Checks whether user input size is invalid compared to legel size.
+     *
+     * @param size upper limit size
+     * @return boolean value indicating validity
+     */
     private boolean invalidSizeComparedTo(int size) {
         return targetIndex.getZeroBased() >= size || targetIndex.getZeroBased() < 0;
     }
 
+    /**
+     * Returns borrowed times of a book.
+     *
+     * @param book the book we aim to check value
+     * @return times value
+     */
     private int getUsage(Book book) {
         return book.getTimes().getValue();
     }
 
+    /**
+     * Implements equals method.
+     *
+     * @param other the other object
+     * @return boolean value, true for equal, false for unequal
+     */
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
