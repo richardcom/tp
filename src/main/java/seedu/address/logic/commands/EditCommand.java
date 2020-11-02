f995eb090373c2049397c288b9995a89051885fe
Jiang <20010421jsy@gmail.com>
2020-11-02 13:59:37 +0800

    Fixed checkstyle issues

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -40,8 +40,8 @@ import seedu.address.ui.Mode;
 public class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
-    public static final String SUGGESTION = "edit <INDEX> <n/NAME> <i/ISBN> <e/EMAIL> <ad/ADDRESS> <c/CATEGORY>… " +
-            "<a/AUTHOR> <p/PUBLISHER> <t/TIMES>";
+    public static final String SUGGESTION = "edit <INDEX> <n/NAME> <i/ISBN> <e/EMAIL> <ad/ADDRESS> <c/CATEGORY>… "
+            + "<a/AUTHOR> <p/PUBLISHER> <t/TIMES>";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the book identified "
             + "by the index number used in the displayed book list. "
