c3c27a4595d0c7bc7ecbf78c92861720c5167cee
richardcom <leonardlym61@gmail.com>
2020-11-05 12:43:21 +0800

    Fix styling errors

--- src/main/java/seedu/address/ui/ProblemReportListCard.java
+++ src/main/java/seedu/address/ui/ProblemReportListCard.java
@@ -1,10 +1,9 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import seedu.address.model.problem.Problem;
 
 public class ProblemReportListCard extends UiPart<Region> {
