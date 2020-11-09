f15ee75836e68f62d277d6b74b0776bf969138a8
richardcom <leonardlym61@gmail.com>
2020-11-09 14:30:56 +0800

    Update reminders

--- src/main/java/seedu/address/model/book/Language.java
+++ src/main/java/seedu/address/model/book/Language.java
@@ -11,8 +11,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Language {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Languages can take only a one-word input without space or symbols"
-            + " containing only characters and it should not be blank";
+            "Languages can only take in alphabets(spaces not allowed) and should not be blank";
 
     public static final String VALIDATION_REGEX = "^[A-Za-z]+$";
 
