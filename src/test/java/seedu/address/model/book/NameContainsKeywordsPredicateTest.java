e52a5cd44474ed6516dafd9031b23c972647b882
Caiyi34777 <e0376971@u.nus.edu>
2020-11-09 13:59:22 +0800

    test

--- src/test/java/seedu/address/model/book/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/book/NameContainsKeywordsPredicateTest.java
@@ -70,6 +70,6 @@ public class NameContainsKeywordsPredicateTest {
         // Keywords match isbn, email and language, but does not match name
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
         assertFalse(predicate.test(new BookBuilder().withName("Alice").withIsbn("12345")
-                .withEmail("alice@email.com").withLanguage("Main Street").build()));
+                .withEmail("alice@email.com").withLanguage("MainStreet").build()));
     }
 }
