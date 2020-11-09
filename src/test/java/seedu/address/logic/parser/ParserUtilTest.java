ae1d014498f9fbcede02c865fcd7cb33b5172288
Caiyi34777 <e0376971@u.nus.edu>
2020-11-09 15:13:40 +0800

    typo

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -8,7 +8,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_BOOK;
 
 import java.util.Arrays;
 import java.util.Collections;
-//import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
